ce0f843a7b9816d981a7e6afbad3e5b9fd06e02b
Alivety <d.yslabs@gmail.com>
2017-06-14 19:54:47 -0400

    Fixed MovementControl

--- src/io/github/alivety/conquerors/client/KeyEvents.java
+++ src/io/github/alivety/conquerors/client/KeyEvents.java
@@ -3,24 +3,23 @@ package io.github.alivety.conquerors.client;
 import java.io.IOException;
 
 import com.jme3.input.controls.ActionListener;
+import com.jme3.input.controls.AnalogListener;
 
 import io.github.alivety.conquerors.common.Main;
 
-public abstract class KeyEvents implements ActionListener {
+public class KeyEvents {
 	public static GameApp app;
 	
 	public static final MovementControl MovementControl=new MovementControl();
 	public static final ExitControl ExitControl=new ExitControl();
 	
-	private static class MovementControl extends KeyEvents {
-		public void onAction(String name, boolean keyPressed, float tpf) {
-			if (!keyPressed) {
-				//TODO movement
-			}
+	private static class MovementControl implements AnalogListener {
+		public void onAnalog(String name, float value, float tpf) {
+			//TODO mvement
 		}
 	}
 	
-	private static class ExitControl extends KeyEvents {
+	private static class ExitControl implements ActionListener {
 		public void onAction(String name, boolean keyPressed, float tpf) {
 			if (!keyPressed) {
 				try {
